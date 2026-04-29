bf3d3d51be65bf5586bee36c19b58a3380289788
siyatut <anastasiya.tyutinova@gmail.com>
2026-04-29 19:28:34 +0300

    refactor: update Android package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.atlantida_shop"
+    namespace = "com.atlantida.mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.atlantida_shop"
+        applicationId = "com.atlantida.mobile"
 
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
--- android/app/src/main/kotlin/com/atlantida/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/atlantida/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.atlantida_shop
+package com.atlantida.mobile
 
 import io.flutter.embedding.android.FlutterActivity
 
